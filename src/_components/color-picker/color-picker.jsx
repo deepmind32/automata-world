fb94e6ac393e7d5f227e56a8ae4499019e0321c4
nshrit <o***@gmail.com>
2025-08-30 18:08:26 +0545

    adds select option

--- src/_components/color-picker/color-picker.jsx
+++ src/_components/color-picker/color-picker.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import styles from "./color-picker.module.css";
 
-export default function ColorPicker({
-	colors,
-	current_color,
-	on_color_selected,
-}) {}
+// colors = object with key (Name) value = color
+export default function ColorPicker({ colors, on_color_selected }) {
+	const [color, set_selected_color] = useState(null);
+
+	return (
+		<div className={styles["color-picker__wrapper"]}>
+			<button className={styles["color-picker__select_btn"]}>
+				{color ?? "Pick an item"}
+			</button>
+
+			<ul className={styles["color-picker__options"]}>
+				{Object.keys(colors).map((item) => (
+					<li key={item}>
+						<button>
+							<span
+								className={styles["color-picker__sample"]}
+								style={{ background: colors[item] }}
+							/>
+							{item}
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { LuBadgeHelp } from "react-icons/lu";
+import { TbSettingsBolt } from "react-icons/tb";
+import { FaPause, FaPlay } from "react-icons/fa";
+
 import Grid from "./_components/grid/grid";
 import styles from "./app.module.css";
+import ColorPicker from "./_components/color-picker/color-picker";
 
 export default function App() {
+	const [current_grid_items, set_current_grid_items] = useState({
+		dead: "red",
+		alive: "blue",
+	});
+
 	return (
-		<main>
-			<Grid />
+		<main className={styles["app"]}>
+			<header className={styles["app__header"]}>
+				{/* <div className={styles["app__header__controls"]}>
+					<button>
+						<FaPlay />
+					</button>
+					<button>
+						<FaPause />
+					</button>
+				</div> */}
+				<ColorPicker colors={current_grid_items} />
+			</header>
+			<Grid size={30} />
+			<div className={styles["app__help__wrapper"]}>
+				<button className={styles["app__fab"]}>
+					<LuBadgeHelp size="3rem" />
+				</button>
+			</div>
+
+			<div className={styles["app__settings__wrapper"]}>
+				<button className={styles["app__fab"]}>
+					<TbSettingsBolt size="3rem" />
+				</button>
+			</div>
 		</main>
 	);
 }
