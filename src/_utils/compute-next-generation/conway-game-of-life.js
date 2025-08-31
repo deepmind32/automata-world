c45f4b9fa698a2fdb7980476edd4b43a4ecb1246
nshrit <o***@gmail.com>
2025-08-31 16:44:23 +0545

    adds conway game of life

--- src/_utils/compute-next-generation/conway-game-of-life.js
+++ src/_utils/compute-next-generation/conway-game-of-life.js
@@ -7,11 +7,37 @@ function copy_grid(grid) {
 export default function compute_next_generation(grid) {
 	const updated_grid = copy_grid(grid);
 
-	console.log(get_neighbours(0, 0, grid.length));
-	// for (let y = 0; y<grid.length; y++) {
-	// 	for (let x=0; x<grid.length; x++) {
+	for (let x = 0; x < grid.length; x++) {
+		for (let y = 0; y < grid.length; y++) {
+			const neighbours = get_neighbours(x, y, grid.length);
+			const neighbours_types = neighbours.map(
+				(coord) => grid[coord[0]][coord[1]]
+			);
+			const neighbours_types_counts = neighbours_types.reduce((acc, item) => {
+				acc[item] = (acc[item] ?? 0) + 1;
+				return acc;
+			}, {});
+			const current_type = grid[x][y];
 
-	// 	}
-	// }
+			if (
+				current_type === "Dead Cell" &&
+				neighbours_types_counts["Alive Cell"] === 3
+			) {
+				updated_grid[x][y] = "Alive Cell";
+			}
+
+			if (current_type === "Alive Cell") {
+				if (
+					neighbours_types_counts["Alive Cell"] == 2 ||
+					neighbours_types_counts["ALive Cell"] == 3
+				) {
+					updated_grid[x][y] = "Alive Cell";
+				} else {
+					updated_grid[x][y] = "Dead Cell";
+				}
+			}
+		}
+	}
+
 	return updated_grid;
 }
